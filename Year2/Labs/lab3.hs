8b4ce8223b554c9bc22074b8d751eb8ad93f3bc5
Mara Pislar <37437735+maraPislar@users.noreply.github.com>
2020-11-27 21:12:01 +0000

    add exercise A8

--- Year2/Labs/lab3.hs
+++ Year2/Labs/lab3.hs
@@ -60,3 +60,41 @@ evalInst :: Stack -> SMProg -> Stack
 evalInst [] _ = error "Cannot operate on an empty stack"
 evalInst x [] = x
 evalInst stack (i:is) = evalInst (eval stack i) is
+
+-- Exercise A8
+
+instructions :: [Instruction]
+instructions = [Pop, Add, Mul, Sub, Div]
+
+findMaxValue :: Stack -> Int -> [Instruction] -> Int
+findMaxValue _ x [] = x
+findMaxValue stack x (i:is)
+    | head (eval stack i) == Nothing = findMaxValue stack x is
+    | x < y = findMaxValue stack y is
+    | otherwise = findMaxValue stack x is
+    where
+        Just y = head (eval stack i)
+
+findMaxInst :: Stack -> [Instruction] -> [Instruction]
+findMaxInst _ [] = []
+findMaxInst stack (i:is)
+    | head (eval stack i) == Nothing = findMaxInst stack is
+    | a == x = i : findMaxInst stack is
+    | otherwise = findMaxInst stack is
+    where
+        Just a = head (eval stack i)
+        x = findMaxValue stack (-1000) instructions
+
+constructList :: Stack -> [[Instruction]]
+constructList stack 
+    | length stack > 1 = findMaxInst stack instructions : constructList newStack
+    | otherwise = []
+    where
+        newStack = eval stack (head (findMaxInst stack instructions))
+
+cartProdN :: [[a]] -> [[a]]
+cartProdN = foldr (\xs as -> [ x : a | x <- xs, a <- as ]) [[]]
+
+findMaxReducers :: Stack -> [SMProg]
+findMaxReducers [] = []
+findMaxReducers stack = cartProdN (constructList stack)

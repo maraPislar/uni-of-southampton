6ebe8e2e30bb0436f9f039228cb77c11d11dac62
Mara Pislar <37437735+maraPislar@users.noreply.github.com>
2020-12-09 19:11:25 +0000

    Add exercise 2

--- Year2/Labs/sheet5.hs
+++ Year2/Labs/sheet5.hs
@@ -9,3 +9,14 @@ occurs x (Node l v r) = case compare x v of
                             EQ -> True
                             LT -> occurs x l
                             GT -> occurs x r
+                            
+-- exercise 2
+-- Note: foldr (\elem acc -> <term>) <start_acc> <list>
+-- Note: foldl (\acc elem -> <term>) <start_acc> <list>
+
+foldTree :: (t1 -> t2) -> (t2 -> t1 -> t2 -> t2) -> Tree t1 -> t2
+foldTree f1 _ (Leaf v) = f1 v
+foldTree f1 f2 (Node l v r) = f2 (foldTree f1 f2 l) v (foldTree f1 f2 r)
+
+flatten :: Tree a -> [a]
+flatten = foldTree (\x -> [x]) (\ls x rs -> ls ++ (x:rs))

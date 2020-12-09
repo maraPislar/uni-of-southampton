1488b19aaaf4920b77dd6ab03a3d1ac0021f08e1
Mara Pislar <37437735+maraPislar@users.noreply.github.com>
2020-12-09 19:35:25 +0000

    add exercise 3

--- Year2/Labs/sheet5.hs
+++ Year2/Labs/sheet5.hs
@@ -20,3 +20,20 @@ foldTree f1 f2 (Node l v r) = f2 (foldTree f1 f2 l) v (foldTree f1 f2 r)
 
 flatten :: Tree a -> [a]
 flatten = foldTree (\x -> [x]) (\ls x rs -> ls ++ (x:rs))
+
+--exercise 3
+
+data Expr = Val Int | Add Expr Expr | Sub Expr Expr
+
+foldExpr :: (Int -> t) -> (t -> t -> t) -> (t -> t -> t) -> Expr -> t
+foldExpr valf _ _ (Val n) = valf n
+foldExpr valf addf subf (Add e1 e2) = addf (foldExpr valf addf subf e1) (foldExpr valf addf subf e2)
+foldExpr valf addf subf (Sub e1 e2) = subf (foldExpr valf addf subf e1) (foldExpr valf addf subf e2)
+
+-- evaluate an expression
+eval :: Expr -> Int
+eval = foldExpr id (+) (-)
+
+-- calculates the size of the AST (in nodes) that represents the expression
+size :: Expr -> Int
+size = foldExpr (const 1) (\x y -> x + y + 1) (\x y -> x + y + 1)

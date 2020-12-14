1a51a6fd3e7f132b16ec59113af5c01b553c05cb
Mara <pislarmara589@gmail.com>
2020-12-14 16:50:47 +0000

    Get random positions from grid.

--- Year2/Coursework/Challenge1/createpuzzle.hs
+++ Year2/Coursework/Challenge1/createpuzzle.hs
@@ -1,10 +1,17 @@
 import Data.List ()
+import System.Random ( Random(randomRIO) )
+import Control.Monad (guard)
 
 type WordSearchGrid = [ [ Char ] ]
 type Placement = (Posn,Orientation)
 type Posn = (Int,Int)
 data Orientation = Forward | Back | Up | Down | UpForward | UpBack | DownForward | DownBack deriving (Eq,Ord,Show,Read)
 
+directions :: [Orientation]
+directions = [Forward, Back, Up, Down, UpForward, UpBack, DownForward, DownBack]
+
+takeXthYth :: [[Char]] -> Int -> Int -> Char
+takeXthYth xs x y = ( xs !! x ) !! y
 
 -- Challenge 2: create a puzzle
 
@@ -13,3 +20,18 @@ sizeofGrid xs d = round (sqrt (fromIntegral numberOfHiddenChars / d))
     where
         numberOfHiddenChars = foldr ((+) . length) 0 xs
 
+createWordSearch :: [ String ] -> Double -> IO Posn
+createWordSearch words density =
+    do
+        let grid = replicate size (replicate size '-')
+        findRandomPositions grid
+    where
+        size = sizeofGrid words density
+
+findRandomPositions :: WordSearchGrid -> IO Posn
+findRandomPositions grid =
+    do 
+        ri <- randomRIO (0, length grid - 1)
+        rj <- randomRIO (0, length grid - 1)
+
+        return (ri, rj)

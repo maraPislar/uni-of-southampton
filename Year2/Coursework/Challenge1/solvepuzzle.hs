4e2bcce8638c53ad970fa1cf51b34c3879512a77
Mara <pislarmara589@gmail.com>
2021-01-09 13:46:25 +0000

    Add some comments and fix minor bugs and add checkers.

--- Year2/Coursework/Challenge1/solvepuzzle.hs
+++ Year2/Coursework/Challenge1/solvepuzzle.hs
@@ -8,6 +8,11 @@ data Orientation = Forward | Back | Up | Down | UpForward | UpBack | DownForward
 
 -- Challenge 1: solve a puzzle
 
+-- directions saved into a list
+directions :: [Orientation]
+directions = [Forward, Back, Up, Down, UpForward, UpBack, DownForward, DownBack]
+
+-- encrypt the direction in number of steps
 encrypt :: Orientation -> Posn
 encrypt o | o == Forward = (0,1)
           | o == Back = (0,-1)
@@ -18,6 +23,7 @@ encrypt o | o == Forward = (0,1)
           | o == DownForward = (1,1)
           | o == DownBack =(1,-1)  
 
+-- decrypt number of steps in direction
 decrypt :: Posn -> Orientation
 decrypt (i,j)
    | i == 0 && j == 1 = Forward
@@ -29,12 +35,15 @@ decrypt (i,j)
    | i == 1 && j == 1 = DownForward
    | i == 1 && j == -1 = DownBack
 
-takeXthYth :: [[Char]] -> Int -> Int -> Char
+-- return a (i, j) element from a matrix
+takeXthYth :: [[a]] -> Int -> Int -> a
 takeXthYth xs x y = ( xs !! x ) !! y
 
+-- check if position of element is within the board
 isGood :: Posn -> Int -> Bool
 isGood (i, j) n = i >= 0 && j >= 0 && j < n && j < n
 
+-- check if word exists at a given position on an ecrypted direction
 checkWord :: String -> WordSearchGrid -> Posn -> Posn -> Posn -> Maybe Placement
 checkWord [] _ (x, y) (_, _) (a, b) = Just ((y, x), decrypt (a, b))
 checkWord word grid (x,y) (i, j) (a, b)
@@ -42,24 +51,24 @@ checkWord word grid (x,y) (i, j) (a, b)
     | takeXthYth grid i j == head word = checkWord (tail word) grid (x,y) (i + a, j + b) (a, b)
     | otherwise = Nothing
 
-directions :: [Orientation]
-directions = [Forward, Back, Up, Down, UpForward, UpBack, DownForward, DownBack]
-
-func1 :: String -> WordSearchGrid -> Posn -> [Orientation] -> Maybe Placement
-func1 _ _ _ [] = Nothing
-func1 word grid (i, j) (d:dx)
-   | checkWord word grid (i, j) (i, j) (encrypt d) == Nothing = func1 word grid (i, j) dx
+-- find a direction on the grid where the word can continue
+findDirection :: String -> WordSearchGrid -> Posn -> [Orientation] -> Maybe Placement
+findDirection _ _ _ [] = Nothing
+findDirection word grid (i, j) (d:dx)
+   | checkWord word grid (i, j) (i, j) (encrypt d) == Nothing = findDirection word grid (i, j) dx
    | otherwise = checkWord word grid (i, j) (i, j) (encrypt d)
 
+-- find where an individual word can be by checking all positions on the board
 solveForWord :: String -> WordSearchGrid -> Int -> Maybe Placement
 solveForWord word grid counter
-   | counter >= ( length grid) * ( length grid ) = Nothing
-   | func1 word grid (i, j) directions == Nothing = solveForWord word grid (counter + 1)
-   | otherwise = func1 word grid (i, j) directions
+   | counter >= n * n = Nothing
+   | findDirection word grid (i, j) directions == Nothing = solveForWord word grid (counter + 1)
+   | otherwise = findDirection word grid (i, j) directions
    where
       n = length grid
       i = counter `div` n
       j = counter `mod` n
 
+-- solve the board for each word on the board
 solveSearch :: [String] -> WordSearchGrid -> [(String, Maybe Placement)]
 solveSearch words grid = [(word, solveForWord word grid 0) | word <- words ]

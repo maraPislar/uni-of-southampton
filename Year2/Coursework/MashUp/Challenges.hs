dc885b5f04fa25a94147cc70446b669dcaa1651a
Mara <pislarmara589@gmail.com>
2021-01-10 13:36:23 +0000

    Add challenge 2

--- Year2/Coursework/MashUp/Challenges.hs
+++ Year2/Coursework/MashUp/Challenges.hs
@@ -123,8 +123,107 @@ solveWordSearch words grid
 
 -- Challenge 2 --
 
+addWord :: String -> WordSearchGrid -> IO WordSearchGrid
+addWord word grid =
+    do
+        ((i, j), dir) <- getRandomPlacement (length grid)
+        if isSafe (length word) (length grid) (i, j) dir && canFit word grid (i, j) dir
+            then return (updateGrid word grid (i, j) dir)
+        else
+            addWord word grid
+
+isSafe :: Int -> Int -> Posn -> Orientation -> Bool
+isSafe wordLen gridLen (i, j) Forward       = j + wordLen <= gridLen
+isSafe wordLen gridLen (i, j) Back          = j - wordLen >= -1
+isSafe wordLen gridLen (i, j) Up            = i - wordLen >= -1
+isSafe wordLen gridLen (i, j) Down          = i + wordLen <= gridLen
+isSafe wordLen gridLen (i, j) UpForward     = i - wordLen >= -1 && j + wordLen <= gridLen
+isSafe wordLen gridLen (i, j) UpBack        = i - wordLen >= -1 && i - wordLen >= -1
+isSafe wordLen gridLen (i, j) DownForward   = i + wordLen <= gridLen && j + wordLen <= gridLen
+isSafe wordLen gridLen (i, j) DownBack      = i + wordLen <= gridLen && j - wordLen >= -1
+
+canFit :: String -> [[Char]] -> Posn -> Orientation -> Bool
+canFit [] _ _ _ = True
+canFit (w:word) grid (i, j) dir
+    | takeXthYth grid i j == w || takeXthYth grid i j == '-' = canFit word grid (i + x, j + y) dir
+    | otherwise = False
+    where
+        (x, y) = encrypt dir
+
+updateGrid :: String -> [[Char]] -> Posn -> Orientation -> [[Char]]
+updateGrid [] grid _ _ = grid
+updateGrid (w:word) grid (i, j) dir = updateGrid word newGrid (i + x, j + y) dir
+    where
+        newGrid = writeOneChar w grid 0 (i, j)
+        (x, y) = encrypt dir
+
+writeOneChar :: Char -> [[Char]] -> Int -> Posn -> [[Char]]
+writeOneChar _ [] _ _ = []
+writeOneChar w (row:grid) counter (i, j)
+    | counter == i = insertAt w j row : writeOneChar w grid (counter + 1) (i, j)
+    | otherwise = row : writeOneChar w grid (counter + 1) (i , j)
+
+insertAt :: a -> Int -> [a] -> [a]
+insertAt newElement _ [] = [newElement]
+insertAt newElement i (a:as)
+  | i <= 0 = newElement:as
+  | otherwise = a : insertAt newElement (i - 1) as
+
+getRandomChar :: [String] -> IO Char
+getRandomChar words =
+    do
+        randomPos <- randomRIO (0, length words - 1)
+        let word = words !! randomPos
+        randomCh <- randomRIO (0, length word - 1)
+        return (word !! randomCh)
+
+getRandomPosition :: Int -> IO Posn
+getRandomPosition len =
+    do 
+        ri <- randomRIO (0, len - 1)
+        rj <- randomRIO (0, len - 1)
+
+        return (ri, rj)
+
+getRandomDirection :: IO Orientation
+getRandomDirection =
+    do
+        pos <- randomRIO (0, 7)
+        return (directions !! pos)
+
+getRandomPlacement :: Int -> IO Placement
+getRandomPlacement len =
+    do
+        pos <- getRandomPosition len
+        dir <- getRandomDirection
+        return (pos, dir)
+
+fillGrid :: [String] -> WordSearchGrid -> WordSearchGrid -> IO WordSearchGrid
+fillGrid _ [] acc = return acc
+fillGrid words (row:grid) acc =
+    do
+        newRow <- fillRow row [] words
+        fillGrid words grid (acc ++ [newRow])
+
+fillRow :: [Char] -> [Char] -> [String] -> IO [Char]
+fillRow [] acc _ = return acc
+fillRow (w:row) acc words =
+    do
+        ch <- getRandomChar words
+        if w == '-'
+            then fillRow row (acc ++ [ch]) words
+        else
+            fillRow row (acc ++ [w]) words
+
 createWordSearch :: [ String ] -> Double -> IO WordSearchGrid
-createWordSearch _ _ = return []
+createWordSearch words density =
+    do
+        let grid = replicate size (replicate size '-')
+        newGrid <- foldM (flip addWord) grid words
+        fillGrid words newGrid []
+    where
+        numberOfHiddenChars = foldr ((+) . length) 0 words
+        size = round (sqrt (fromIntegral numberOfHiddenChars / density))
 
 
 --- Convenience functions supplied for testing purposes
